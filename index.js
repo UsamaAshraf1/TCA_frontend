b73a7ed1b1b943d0c21c26eb0be62d025826b162
USAMA ASHRAF <usamaashraf728@gmail.com>
2025-02-21 13:20:27 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+let toggleBtn = document.querySelector("#navbar-toggle");
+let collapse = document.querySelector("#navbar-collapse");
+
+toggleBtn.onclick = () => {
+  collapse.classList.toggle("hidden");
+  collapse.classList.toggle("flex");
+};
+
+// document.addEventListener("scroll", function () {
+//   let scrollY = window.scrollY;
+//   let textElement = document.getElementById("scrollText");
+
+//   // Moves the text up as the user scrolls
+//   textElement.style.transform = `translateY(-${scrollY * 0.4}px)`;
+// });
+
+document.addEventListener("scroll", function () {
+  let scrollY = window.scrollY;
+  let textElement = document.getElementById("scrollText");
+
+  // Calculate translation but limit it to a max of -556px
+  let translateY = Math.min(scrollY * 0.4, 590);
+
+  // Apply transform
+  textElement.style.transform = `translateY(-${translateY}px)`;
+});
